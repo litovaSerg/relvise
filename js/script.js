10723176807f588dcf894bb870c4a2d739d29205
Serg <sergei.lsn@mail.ru>
2022-10-20 11:14:58 +0300

    added modal

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
+  // * Switch tabs
   const servicesColumn = document.querySelectorAll('.services__item');
   const getInTouch = document.querySelectorAll('.getintouch__item');
   const serviceHover = 'item-service--green';
   const getInTouchHover = 'item-getintouch--active';
-  console.log(getInTouch);
 
   function hideBackgroundTab(block, activeStyle) {
     block.forEach((tab) => {
@@ -12,23 +12,50 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  function showBackgroundTab() {
-    servicesColumn.forEach((tab) => {
-      tab.classList.add(serviceHover);
-    });
+  function showBackgroundTab(block, activeStyle) {
+    block.classList.add(activeStyle);
   }
 
-  servicesColumn.forEach((tabs, index) => {
-    tabs.addEventListener('click', () => {
-      hideBackgroundTab(servicesColumn, serviceHover);
-      servicesColumn[index].classList.add(serviceHover);
+  function switchTabs(block, activeStyle) {
+    block.forEach((tab, index) => {
+      tab.addEventListener('click', () => {
+        hideBackgroundTab(block, activeStyle);
+        showBackgroundTab(block[index], activeStyle);
+      });
     });
+  }
+  switchTabs(servicesColumn, serviceHover);
+  switchTabs(getInTouch, getInTouchHover);
+
+  // * Modal window
+  const contact = document.querySelectorAll('[data-contact]');
+  const modalWindow = document.querySelector('.modal');
+  const closeModalWindow = document.querySelector('[data-close]');
+
+  function openModal() {
+    modalWindow.classList.remove('modal_hide');
+    modalWindow.classList.add('modal_show');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function closeModal() {
+    modalWindow.classList.remove('modal_show');
+    modalWindow.classList.add('modal_hide');
+    document.body.style.overflow = '';
+  }
+
+  contact.forEach((link) => {
+    link.addEventListener('click', openModal);
   });
 
-  getInTouch.forEach((tabs, index) => {
-    tabs.addEventListener('click', () => {
-      hideBackgroundTab(getInTouch, getInTouchHover);
-      getInTouch[index].classList.add(getInTouchHover);
-    });
-  });
+  closeModalWindow.addEventListener('click', closeModal);
+
+  // * Scroll open modal window
+  function showMOdalScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showMOdalScroll);
+    }
+  }
+  window.addEventListener('scroll', showMOdalScroll);
 });
